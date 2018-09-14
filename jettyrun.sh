a8385bd0d36dfcb1cb60a85a77654521aae31aa7
youken9980 <youken9980@163.com>
2018-09-14 08:28:54 +0800

    更新脚本

--- jettyrun.sh
+++ jettyrun.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-~/mvn.sh jetty:run
+file_name="$(pwd)"
+file_name="${file_name##*/}.log"
+
+nohup mvn clean jetty:run -Dmaven.test.skip=true -U >> "${file_name}" 2>&1 &
+tail -f "${file_name}"
--- springbootrun.sh
+++ springbootrun.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-~/mvn.sh spring-boot:run
+file_name="$(pwd)"
+file_name="${file_name##*/}.log"
+
+nohup mvn clean spring-boot:run -Dmaven.test.skip=true -U >> "${file_name}" 2>&1 &
+tail -f "${file_name}"
