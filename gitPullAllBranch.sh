464682b799aeb9210fa32b430786ea994d378c47
youken9980 <youken9980@163.com>
2018-10-31 11:06:47 +0800

    更新脚本

--- gitPullAllBranch.sh
+++ gitPullAllBranch.sh
@@ -5,15 +5,8 @@ current_branch="$(git symbolic-ref --short -q HEAD)"
 git checkout master
 git pull --rebase
 
-# for item in $(git branch | grep -v master); do
-# 	git branch -D "${item}"
-# done
-
 git remote prune origin
-for item in $(git branch -r | grep -v '\->' | sed 's/origin\///g'); do
-	if [[ "${item}" == "${current_branch}" ]]; then
-		continue;
-	fi
+for item in $(git branch -r | grep -v '\->' | grep -v 'master' | sed 's/origin\///g'); do
  	git checkout "${item}"
  	git pull --rebase
 done
