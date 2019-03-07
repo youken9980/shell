3bb627ba7686525cd416f687b953b2a5a49d72b7
youken9980 <youken9980@163.com>
2019-03-07 08:45:16 +0800

    更新脚本

--- dockerRunRedis.sh
+++ dockerRunRedis.sh
@@ -3,4 +3,4 @@
 docker run -d -p 6379:6379 \
   -v ~/dockerVolumn/redis/data:/data \
   --network mynet --name redis-local \
-  redis:3 redis-server --appendonly yes
+  redis redis-server --appendonly yes
--- dockerRunRedisClient.sh
+++ dockerRunRedisClient.sh
@@ -2,4 +2,4 @@
 
 docker run -it --rm \
   --network mynet --name redis-client \
-  redis:3 redis-cli -h redis-local -p 6379 --raw
+  redis redis-cli -h redis-local -p 6379 --raw
