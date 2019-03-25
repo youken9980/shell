10053ca5854ff8ad1aa482c6c0e00146a56e6124
youken9980 <youken9980@163.com>
2019-03-25 10:10:31 +0800

    更新脚本

--- dockerupdate.sh
+++ dockerupdate.sh
@@ -4,6 +4,7 @@ images="docker images -f dangling=false | grep -v 'REPOSITORY' | grep -v '<none>
 images="${images} | grep -v 'local/'"
 images="${images} | grep -v 'reg.enncloud.cn/'"
 images="${images} | grep -v 'smartcloud/'"
+images="${images} | grep -v 'chaoscloud/'"
 eval "${images}" | while read img; do
 	repo="$(echo ${img} | awk '{print $1":"$2}')"
 	size="$(echo ${img} | awk '{print $7}')"
