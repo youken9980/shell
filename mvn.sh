3b0b4d87535b3d6a1999cec16f6c0dba8da09fe6
youken9980 <youken9980@163.com>
2019-05-28 13:01:25 +0800

    更新脚本

--- mvn.sh
+++ mvn.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 # 集合列表
-arg_list="-X clean compile package install deploy spring-boot:run jetty:run docker:build dockerfile:build"
+arg_list="clean compile package install deploy spring-boot:run jetty:run docker:build dockerfile:build -P -X"
 for ((i=1; i<=$#; i++)); do
-	# echo "$(eval echo '$'"$i")"
-	if [[ ! " ${arg_list} " =~ " $(eval echo '$'"$i") " ]]; then
-		# 参数错误则提示命令行使用方法，sed命令将arg_list中的空格替换为竖线
-		echo "Usage: $0 [$(echo ${arg_list} | sed 's/ /|/g')]"
-		exit 1
+	arg="$(eval echo '$'"$i")"
+	if [[ ! "${arg_list}" =~ "${arg}" ]]; then
+        if [[ "${arg:0:2}" != "-P" ]]; then
+            # echo "${arg} -> ${arg:0:2}"
+            # 参数错误则提示命令行使用方法，sed命令将arg_list中的空格替换为竖线
+            echo "Usage: $0 [$(echo ${arg_list} | sed 's/ /|/g')]"
+            exit 1
+        fi
 	fi
 done
 
