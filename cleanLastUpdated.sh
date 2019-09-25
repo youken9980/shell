1e59b34c5e8066ae5aee875c9af671a284b11660
youken9980 <youken9980@163.com>
2019-09-25 14:52:10 +0800

    更新脚本

--- cleanLastUpdated.sh
+++ cleanLastUpdated.sh
@@ -4,4 +4,6 @@ repo_path=/Volumes/Destiny/Share/maven-repository
 echo 正在搜索...
 find ${repo_path} -name "*lastUpdated*"
 find ${repo_path} -name "*lastUpdated*" | xargs rm -fr
+find ${repo_path} -name "\$\{*"
+find ${repo_path} -name "\$\{*" | xargs rm -rf
 echo 搜索完毕
