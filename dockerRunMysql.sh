e6b185e187741efc658f72bf0aed1be1f6da64b5
youken9980 <youken9980@163.com>
2019-10-09 11:09:17 +0800

    更新脚本

--- dockerRunMysql.sh
+++ dockerRunMysql.sh
@@ -6,10 +6,12 @@ if [ $# == 1 ]; then
 fi
 
 data_path="~/dockerVolumn/mysql/data/"
+logs_path="~/dockerVolumn/mysql/logs/"
 
 eval "docker run -d -p 3306:3306 \
   -v ${data_path}${default}:/var/lib/mysql \
   -v ~/dockerVolumn/mysql/config/mysql.cnf:/etc/mysql/mysql.cnf \
+  -v ${logs_path}${default}/mysql-slow.log:/etc/mysql/logs/mysql-slow.log \
   --network mynet --name mysql-${default} \
   -e MYSQL_ROOT_PASSWORD=admin123 mysql:5 \
   --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci"
