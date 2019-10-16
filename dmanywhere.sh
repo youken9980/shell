e0beff7459e5a5b2e8e8e673316c1bd173927973
youken9980 <youken9980@163.com>
2019-10-16 19:10:09 +0800

    更新脚本

--- dmanywhere.sh
+++ dmanywhere.sh
@@ -5,7 +5,9 @@ default="9090"
 if [ $# == 1 ]; then
     default="$1"
 fi
+log_file_path="/tmp/dmanywhere.log"
 
-nohup dmanywhere -p ${default} > /dev/null 2>&1 &
+nohup dmanywhere -p ${default} > ${log_file_path} 2>&1 &
 sleep 1s
-open -a Google\ Chrome http://localhost:${default}
+open -a "Google Chrome" http://localhost:${default}
+tail -f ${log_file_path}
