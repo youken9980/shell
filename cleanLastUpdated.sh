9fb2070abd346ca25e27cb87a81636395c2dbffe
youken9980 <youken9980@163.com>
2020-03-30 10:47:06 +0800

    更新脚本

--- cleanLastUpdated.sh
+++ cleanLastUpdated.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 repo_path=~/Destiny/Share/maven-repository
+pattern_list="
+*.lastUpdated
+\$\{*
+"
+
 echo 正在搜索...
-find ${repo_path} -name "*.lastUpdated"
-find ${repo_path} -name "*.lastUpdated" -print0 | xargs -0 rm
-find ${repo_path} -name "\$\{*"
-find ${repo_path} -name "\$\{*" -print0 | xargs -0 rm
+for item in ${pattern_list}; do
+    find "${repo_path}" -name "${item}" -type f -print -exec rm {} \;
+done
 echo 搜索完毕
