a8a6cbfe631353761254c353e49f777ea0b29ebc
youken9980 <youken9980@163.com>
2020-12-04 08:27:02 +0800

    更新脚本

--- dockerlsimg.sh
+++ dockerlsimg.sh
@@ -5,7 +5,5 @@ if [ $# -gt 0 ]
 then
 	args="| $1"
 fi
-images="docker images -f dangling=false | grep -v 'REPOSITORY' | grep -v '<none>' $args"
-eval "${images}" | while read img; do
- 	echo "$(echo ${img} | awk '{print $1":"$2"    "$3"    "$7}')"
-done
+
+docker images --format "table {{.Repository}}:{{.Tag}}\t{{.Size}}\t{{.CreatedSince}}" -f dangling=false | grep -v 'REPOSITORY' | grep -v '<none>' ${args}
