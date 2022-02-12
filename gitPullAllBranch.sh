d685a0d68d559e6b995e2a4a52447082ec2103de
youken9980 <youken9980@163.com>
2022-02-12 09:25:26 +0800

    更新脚本

--- gitPullAllBranch.sh
+++ gitPullAllBranch.sh
@@ -5,11 +5,13 @@ default_branch="$(git branch -r | grep '\->' | awk '{ print $3 }' | sed 's/origi
 
 git checkout "${default_branch}"
 git pull --rebase
+git remote prune origin
 
 git remote prune origin
 for item in $(git branch -r | grep -v '\->' | sed 's/origin\///g'); do
  	git checkout "${item}"
  	git pull --rebase
+	git gc --prune=now
 done
 
 git checkout "${current_branch}"
