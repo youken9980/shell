ba43f65222cd34577259b6bc71d97d2805846319
youken9980 <youken9980@163.com>
2023-03-01 09:52:11 +0800

    更新脚本

--- dockerRunGogs.sh
+++ dockerRunGogs.sh
@@ -6,13 +6,13 @@ docker run -d --expose 3306 \
   -e TZ="Asia/Shanghai" \
   -e MYSQL_ROOT_PASSWORD=admin123 \
   --network mynet --name mysql-gogs \
-  mysql:5 \
+  mysql:5.7-debian \
   --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
 
 docker run -d -p 3000:3000 \
   -e TZ="Asia/Shanghai" \
   -v ~/dockerVolume/gogs/data:/data \
   --network mynet --name gogs \
-  gogs/gogs
+  gogs/gogs:latest
 
 docker logs -f gogs
