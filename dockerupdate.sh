fedc5c7d4efdc6800c8f954b23421d3c86b54f37
youken9980 <youken9980@163.com>
2025-04-04 10:23:38 +0800

    更新脚本

--- dockerupdate.sh
+++ dockerupdate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 images="docker images -f dangling=false | grep -v 'REPOSITORY' | grep -v '<none>'"
-images="${images} | grep -v 'localhost:15000/'"
+images="${images} | grep -v 'localhost:'"
 images="${images} | grep -v 'local/'"
 images="${images} | grep -v 'youken9980/'"
 images="${images} | grep -v 'reg.enncloud.cn/'"
@@ -10,6 +10,7 @@ images="${images} | grep -v 'chaoscloud/'"
 images="${images} | grep -v 'springcloud/'"
 images="${images} | grep -v 'eo/code'"
 images="${images} | grep -v 'sc/code'"
+images="${images} | grep -v 'firecrawl-'"
 eval "${images}" | while read img; do
 	repo="$(echo ${img} | awk '{print $1":"$2}')"
 	size="$(echo ${img} | awk '{print $7}')"
