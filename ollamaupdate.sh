492ae756cc39bf27b5ed47da30d69c7d1a54a583
youken9980 <youken9980@163.com>
2025-08-23 09:51:21 +0800

    更新脚本

---
+++ ollamaupdate.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+models="ollama list | grep -v 'SIZE'"
+models="${models} | grep -v 'youken9980/'"
+models="${models} | grep -v 'unsloth/'"
+models="${models} | sort -hr"
+eval "${models}" | while read line; do
+	model="$(echo ${line} | awk '{print $1}')"
+	size="$(echo ${line} | awk '{print $3" "$4}')"
+	cmd="ollama pull ${model}"
+	echo -e "${size}\t${cmd}"
+	eval "${cmd}"
+	echo ""
+done
+echo ""
+ollama list
