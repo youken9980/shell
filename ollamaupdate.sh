baadd6f7ec77765a03b2992471b14665f25df0a5
youken9980 <youken9980@163.com>
2025-09-05 11:04:50 +0800

    更新脚本

--- ollamaupdate.sh
+++ ollamaupdate.sh
@@ -4,7 +4,7 @@
 models="ollama list | grep -v 'SIZE'"
 models="${models} | grep -v 'youken9980/'"
 models="${models} | grep -v 'unsloth/'"
-models="${models} | sort -hr"
+# models="${models} | sort -hr"
 eval "${models}" | while read line; do
 	model="$(echo ${line} | awk '{print $1}')"
 	size="$(echo ${line} | awk '{print $3" "$4}')"
